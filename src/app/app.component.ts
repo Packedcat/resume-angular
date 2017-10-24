05c418047698750c86dffa22638e9fb5cfbf2fd9
Yuumao <xc648593557@gmail.com>
2017-10-24 14:57:23 +0800

    [fix] 'Expression Changed After It Has Been Checked'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,78 +1,94 @@
-import { Component } from '@angular/core';
-import { fullCode } from './full-code';
+import { Component, ChangeDetectorRef, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+import { ResumePanelComponent } from './resume-panel.component';
+import { CodeEditorComponent } from './code-editor.component';
 import { fullResume } from './full-resume';
+import { fullCode } from './full-code';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  interval = 40;
+export class AppComponent implements OnInit, AfterViewInit {
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  @ViewChild(CodeEditorComponent)
+  private codeComponent: CodeEditorComponent;
+  @ViewChild(ResumePanelComponent)
+  private resumeComponent: ResumePanelComponent;
+
+  interval = 10;
   enableHtml = false;
-  currentCode = fullCode[0];
+  currentCode = '';
   fullCode = fullCode;
   currentResume = '';
   fullResume = fullResume;
 
-  // async makeResume(): Promise<void> {
-  //   await this.progressivelyShowCode(0)
-  //   // await this.progressivelyShowResume()
-  //   // await this.progressivelyShowCode(1)
-  //   await this.showHtml()
-  //   // await this.progressivelyShowCode(2)
-  // }
+  async makeResume(): Promise<void> {
+    await this.progressivelyShowCode(0)
+    await this.progressivelyShowResume()
+    await this.progressivelyShowCode(1)
+    await this.showHtml()
+    await this.progressivelyShowCode(2)
+  }
 
-  // showHtml(): Promise<any> {
-  //   return new Promise(resolve => {
-  //     this.enableHtml = true
-  //     resolve()
-  //   })
-  // }
+  showHtml(): Promise<void> {
+    return new Promise<void>(resolve => {
+      this.enableHtml = true
+      resolve()
+    })
+  }
 
-  // progressivelyShowCode(n: number): Promise<any> {
-  //   return new Promise(resolve => {
-  //     let interval = this.interval
-  //     let showStyle = (async function() {
-  //       let code = this.fullCode[n]
-  //       let length = this.fullCode.filter((_, index) => index <= n).map((item) => item.length).reduce((p, c) => p + c, 0)
-  //       let prefixLength = length - code.length
-  //       if (this.currentCode.length < length) {
-  //         let l = this.currentCode.length - prefixLength
-  //         let char = code.substring(l, l + 1) || ' '
-  //         this.currentCode += char
-  //         // if (code.substring(l - 1, l) === '\n' && this.$refs.styleEditor) {
-  //         //   this.$nextTick(() => {
-  //         //     this.$refs.styleEditor.goBottom()
-  //         //   })
-  //         // }
-  //         setTimeout(showStyle, interval)
-  //       } else {
-  //         resolve()
-  //       }
-  //     }).bind(this)
-  //     showStyle()
-  //   })
-  // }
+  progressivelyShowCode(n: number): Promise<void> {
+    return new Promise<void>(resolve => {
+      let interval = this.interval
+      let showStyle = (async function() {
+        let code = this.fullCode[n]
+        let length = this.fullCode.filter((_, index) => index <= n).map((item) => item.length).reduce((p, c) => p + c, 0)
+        let prefixLength = length - code.length
+        if (this.currentCode.length < length) {
+          let l = this.currentCode.length - prefixLength
+          let char = code.substring(l, l + 1) || ' '
+          this.currentCode += char
+          if (code.substring(l - 1, l) === '\n') {
+            setTimeout(() => this.codeComponent.goBottom(), 0);
+          }
+          setTimeout(showStyle, interval)
+        } else {
+          resolve()
+        }
+      }).bind(this)
+      showStyle()
+    })
+  }
 
-  // progressivelyShowResume(): Promise < any > {
-  //   return new Promise((resolve, reject) => {
-  //     let length = this.fullResume.length
-  //     let interval = this.interval
-  //     let showResume = () => {
-  //       if (this.currentResume.length < length) {
-  //         this.currentResume = this.fullResume.substring(0, this.currentResume.length + 1)
-  //         let lastChar = this.currentResume[this.currentResume.length - 1]
-  //         let prevChar = this.currentResume[this.currentResume.length - 2]
-  //         if (prevChar === '\n' && this.$refs.resumeEditor) {
-  //           this.$nextTick(() => this.$refs.resumeEditor.goBottom())
-  //         }
-  //         setTimeout(showResume, interval)
-  //       } else {
-  //         resolve()
-  //       }
-  //     }
-  //     showResume()
-  //   })
-  // }
+  progressivelyShowResume(): Promise<void> {
+    return new Promise<void>(resolve => {
+      let length = this.fullResume.length
+      let interval = this.interval
+      let showResume = () => {
+        if (this.currentResume.length < length) {
+          this.currentResume = this.fullResume.substring(0, this.currentResume.length + 1)
+          let lastChar = this.currentResume[this.currentResume.length - 1]
+          let prevChar = this.currentResume[this.currentResume.length - 2]
+          if (prevChar === '\n') {
+            setTimeout(() => this.resumeComponent.goBottom(), 0);
+          }
+          setTimeout(showResume, interval)
+        } else {
+          resolve()
+        }
+      }
+      showResume()
+    })
+  }
+
+  ngOnInit() {
+    this.cdr.detach();
+  }
+
+  ngAfterViewInit(): void {
+    this.makeResume()
+    setTimeout(() => this.cdr.reattach());
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+// import { enableProdMode } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CodeEditorComponent } from './code-editor.component';
+import { ResumePanelComponent } from './resume-panel.component';
 import { SafeHtmlPipe } from './safe-html.pipe';
+
+// enableProdMode()
 
 @NgModule({
   declarations: [
     AppComponent,
     CodeEditorComponent,
+    ResumePanelComponent,
     SafeHtmlPipe,
   ],
   imports: [
